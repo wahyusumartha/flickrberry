a947f100a461cc2cc0003959bb1a249ae9f4de78
wahyusumartha <wahyusumartha@gmail.com>
2010-10-30 01:40:03 +0700

    adding interface for method flickr.auth.getFullToken

--- com/flickberry/flickr/FlickrAuth.java
+++ com/flickberry/flickr/FlickrAuth.java
@@ -1,12 +1,22 @@
 package com.flickberry.flickr;
 
-import com.flickberry.json.JSONObject;
-
+/*
+ * This is using to handle method flickr.auth.getFullToken 
+ */
 public interface FlickrAuth {
+	/*
+	 * Get Authentication Token a.k.a auth_token
+	 */
 	public String getToken();
 
+	/*
+	 * Get Permission When using your application a.k.a perms
+	 */
 	public String getPerms();
-	
+
+	/*
+	 * Get Your User Id
+	 */
 	public String userId();
 
 }
